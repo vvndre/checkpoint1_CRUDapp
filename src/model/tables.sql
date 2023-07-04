aff02a9822535f846ed6260be35230b01a9cc903
andrercastillon <andrercastillon@gmail.com>
2023-07-03 19:04:02 -0500

    updated sql table

--- src/model/tables.sql
+++ src/model/tables.sql
@@ -2,6 +2,6 @@ create table movies (
  id integer primary key auto_increment,
    movie_name varchar(100),
    movie_year integer,
-   genre varchar(100) NOT NULL, 
+   genre varchar(100), 
    finished boolean default false
 );
